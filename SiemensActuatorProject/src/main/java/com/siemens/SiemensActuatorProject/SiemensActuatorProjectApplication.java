b6636976ddf68d6d0050b1471c8cf14067e708dd
chintanKilledar <chintan.killedar@siemens.com>
2018-10-09 12:48:02 +0530

    actuator code

---
+++ SiemensActuatorProject/src/main/java/com/siemens/SiemensActuatorProject/SiemensActuatorProjectApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.SiemensActuatorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemensActuatorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiemensActuatorProjectApplication.class, args);
+	}
+}
